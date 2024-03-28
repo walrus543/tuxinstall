8fc6a3ae8387f210ca8983874104fd325933a407
walrus <github.urljk@8alias.com>
2024-03-28 17:12:45 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,8 +11,8 @@ echo Assistant pour reconfigurer ARCH Plasma après un formatage
 echo ----------------------------------------------------
 
 echo Config pacman
-sudo sed -i '/^#ParallelDownloads/a ILoveCandy' /etc/pacman.conf
-sudo sed -i '/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
+sudo sed -i 's/^#ParallelDownloads/a ILoveCandy' /etc/pacman.conf
+sudo sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 sudo sed -i 's/^#Color/Color/' /etc/pacman.conf
 
 echo Actualisation des dépôts et mises à jour
@@ -65,15 +65,20 @@ sudo pacman -S --needed bat btop duf element-desktop eza syncthing fastfetch fir
 echo Installation de divers paquets propres à Arch
 sudo pacman -S --needed adobe-source-han-sans-cn-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts android-tools cups dkms dosfstools firefox flatpak gwenview jre-openjdk-headless kcalc kimageformats kwallet libreoffice-{fresh,fresh-fr} linux-lts-headers man-pages ntfs-3g okular p7zip pacman-contrib perl-rename pkgfile print-manager qt5-imageformats xdg-desktop-portal-gtk
 
-echo Installation de paru
-sudo pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
-
-echo Paru NewsOnUpdate
-sudo sed -i '/^#NewsOnUpdate/NewsOnUpdate/' /etc/paru.conf
-
+paru --version; if echo $? = 0;
+then
+    echo paru déjà installé
+else
+    echo Installation de paru
+    sudo pacman -S --needed git base-devel
+    git clone https://aur.archlinux.org/paru.git
+    cd paru
+    makepkg -si
+fi
+    # Que paru soit installé ou pas, on contrôle les news
+    echo Paru NewsOnUpdate
+    sudo sed -i '/^#NewsOnUpdate/NewsOnUpdate/' /etc/paru.conf
+    
 echo Installation de paquets avec paru
 paru -S --needed brave-bin cnijfilter2-mg7500 downgrade payload-dumper-go-bin protonmail-bridge-bin reflector-simple rtl8821ce-dkms-git uniutils pika-backup
 
@@ -100,19 +105,18 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 
 echo Installation de ZSH et configuration
 sudo pacman -S --needed zsh
-#definir zsh par défaut
 
-echo Installation Oh My ZSH
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo Oh My ZSH
+[[ -d ~/.oh-my-zsh ]] && echo Oh My ZSH déjà installé || sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo Installation zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+[[ -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]] && echo zsh-autosuggestions déjà installé || git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 echo Installation zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+[[ -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]] && echo zsh-syntax-highlighting déjà installé || git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 echo Installation du thème powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+[[ -d ~/.oh-my-zsh/custom/themes/powerlevel10k ]] && echo powerlevel10k déjà installé || git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 echo Désactiver le bruit lors de la recherche
 echo "blacklist pcspkr" | sudo tee -a /etc/modprobe.d/nobeep.conf > /dev/null
@@ -135,8 +139,8 @@ EOF
 
 echo "source $HOME/.bash_aliases" | sudo tee -a ~/.zshrc > /dev/null
 echo "alias lsl='eza -la --color=always --group-directories-first'" | sudo tee -a ~/.zshrc > /dev/null
-sed 's/^ZSH_THEME.*$/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/' ~/.zshrc
-sed 's/^plugins=.*$/plugins=(\ngit\nzsh-autosuggestions\nzsh-syntax-highlighting\n)/' ~/.zshrc
+sed -i 's/^ZSH_THEME.*$/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/' ~/.zshrc
+sed -i 's/^plugins=.*$/plugins=(\ngit\nzsh-autosuggestions\nzsh-syntax-highlighting\n)/' ~/.zshrc
 
 echo Nettoyage de tuxinstall
 rm -rf ~/tuxinstall
