c70e58f37ef9290ace84f90bbf510babbae86a5a
walrus <github.urljk@8alias.com>
2024-03-29 11:13:10 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,31 +14,31 @@ export YELLOW=$(tput setaf 3)
 export BLUE=$(tput setaf 4)
 
 echo ""
-echo ----------------------------------------------------
+echo ${BLUE}----------------------------------------------------
 echo Assistant pour reconfigurer ARCH Plasma après un formatage
-echo ----------------------------------------------------
+echo ----------------------------------------------------${RESET}
 echo ""
 
 # get confirmation
 read -t 15 -N 1 -p "Prêt à faire la post install de Arch sur KDE Plasma?. On continue? (y/N) " start
 echo 
  
-# if answer is yes within 15 seconds start updating cluster nodes ...
+# if answer is yes within 15 seconds start installing...
 if [ "${start,,}" == "y" ]
 then
-    echo Config pacman
+    echo ${BLUE}Config pacman${RESET}
     sudo sed -i '/^#ParallelDownloads/a ILoveCandy' /etc/pacman.conf
     sudo sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
     sudo sed -i 's/^#Color/Color/' /etc/pacman.conf
     
-    echo Actualisation des dépôts et mises à jour
+    echo ${BLUE}Actualisation des dépôts et mises à jour${RESET}
     sudo pacman -Syu
     
-    echo Installation de paquets relatifs à OSHeden
+    echo ${BLUE}Installation de paquets relatifs à OSHeden${RESET}
     sudo pacman -S --needed dos2unix xclip xsel npm fdupes
     
     if [ -d ~/AndroidAll/Thèmes_Shorts/ ]
-        then echo Creation des liens symboliques pour les thèmes
+        then echo ${BLUE}Creation des liens symboliques pour les thèmes${RESET}
         ln -s /home/arnaud/Thèmes/Alta/app/src/main/ /AndroidAll/Thèmes_Shorts/Alta
         ln -s /home/arnaud/Thèmes/Altess/app/src/main /AndroidAll/Thèmes_Shorts/Altess
         ln -s /home/arnaud/Thèmes/Azulox/app/src/main/ /AndroidAll/Thèmes_Shorts/Azulox
@@ -75,107 +75,107 @@ then
         ln -s /home/arnaud/Thèmes/Whity/app/src/main/ /AndroidAll/Thèmes_Shorts/Whity
     fi
     
-    echo Installation de divers utilitaires généraux
+    echo ${BLUE}Installation de divers utilitaires généraux${RESET}
     sudo pacman -S --needed bat btop duf element-desktop eza syncthing fastfetch firefox flameshot kdeconnect kio-admin meld ncdu obsidian pdfarranger samba simple-scan smbclient systemdgenie telegram-desktop thunar thunderbird timeshift transmission-qt yt-dlp
     
-    echo Installation de divers paquets propres à Arch
+    echo ${BLUE}Installation de divers paquets propres à Arch${RESET}
     sudo pacman -S --needed adobe-source-han-sans-cn-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts android-tools cups dkms dosfstools firefox flatpak gwenview jre-openjdk-headless kcalc kimageformats kwallet libreoffice-{fresh,fresh-fr} linux-lts-headers man-pages ntfs-3g okular p7zip pacman-contrib perl-rename pkgfile print-manager qt5-imageformats xdg-desktop-portal-gtk
     
     paru --version; if echo $? = 0;
     then
-        echo paru déjà installé
+        echo ${GREEN}paru déjà installé${RESET}
     else
-        echo Installation de paru
+        echo ${BLUE}Installation de paru${RESET}
         sudo pacman -S --needed git base-devel
         git clone https://aur.archlinux.org/paru.git
         cd paru
         makepkg -si
     fi
         # Que paru soit installé ou pas, on contrôle les news
-        echo Paru NewsOnUpdate
+        echo ${BLUE}Paru NewsOnUpdate${RESET}
         sudo sed -i 's/^#NewsOnUpdate/NewsOnUpdate/' /etc/paru.conf
         
-    echo Installation de paquets avec paru
+    echo ${BLUE}Installation de paquets avec paru${RESET}
     paru -S --needed brave-bin cnijfilter2-mg7500 downgrade payload-dumper-go-bin protonmail-bridge-bin reflector-simple rtl8821ce-dkms-git uniutils pika-backup
     
-    echo Gestion de la carte réseau Realtek
+    echo ${BLUE}Gestion de la carte réseau Realtek${RESET}
     echo "# https://github.com/tomaspinho/rtl8821ce/tree/master#wi-fi-not-working-for-kernel--59" | sudo tee -a /etc/modprobe.d/blacklist.conf > /dev/null
     echo "blacklist rtw88_8821ce" | sudo tee -a /etc/modprobe.d/blacklist.conf > /dev/null
     
-    echo "Activation de l'imprimante et du bluetooth au démarrage"
+    echo ${BLUE}"Activation de l'imprimante et du bluetooth au démarrage"${RESET}
     sudo systemctl enable --now cups.socket
     sudo systemctl enable cups.service
     sudo systemctl enable --now bluetooth.service
     
-    echo Installation de VirtualBox pour linux et linux-lts
-    sudo pacman -S --needed virtualbox virtualbox-guest-iso virtualbox-host-modules-arch virtualbox-host-dkms
+    echo ${BLUE}Installation de VirtualBox pour linux et linux-lts${RESET}
+    sudo pacman -S --needed virtualbox virtualbox-guest-iso virtualbox-host-dkms
     
-    echo Activation du nettoyage du cache des paquets
+    echo ${BLUE}Activation du nettoyage du cache des paquets${RESET}
     sudo systemctl enable paccache.timer
     
-    echo Installation de paquets pour carte graphique NVIDIA
+    echo ${BLUE}Installation de paquets pour carte graphique NVIDIA${RESET}
     sudo pacman -S --needed nvidia nvidia-lts nvidia-utils nvidia-settings vulkan-icd-loader
     
-    echo Installation du dépôt officiel Flatpak
+    echo ${BLUE}Installation du dépôt officiel Flatpak${RESET}
     flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
     
-    echo Installation de ZSH et configuration
+    echo ${BLUE}Installation de ZSH et configuration${RESET}
     sudo pacman -S --needed zsh
     
-    echo Oh My ZSH
-    [[ -d ~/.oh-my-zsh ]] && echo Oh My ZSH déjà installé || sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    echo ${BLUE}Oh My ZSH${RESET}
+    [[ -d ~/.oh-my-zsh ]] && echo ${GREEN}Oh My ZSH déjà installé${RESET} || sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     
-    echo Installation zsh-autosuggestions
-    [[ -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]] && echo zsh-autosuggestions déjà installé || git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    echo ${BLUE}Installation zsh-autosuggestions${RESET}
+    [[ -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]] && echo ${GREEN}zsh-autosuggestions déjà installé${RESET} || git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     
-    echo Installation zsh-syntax-highlighting
-    [[ -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]] && echo zsh-syntax-highlighting déjà installé || git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    echo ${BLUE}Installation zsh-syntax-highlighting${RESET}
+    [[ -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]] && echo ${GREEN}zsh-syntax-highlighting déjà installé${RESET} || git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     
-    echo Installation du thème powerlevel10k
-    [[ -d ~/.oh-my-zsh/custom/themes/powerlevel10k ]] && echo powerlevel10k déjà installé || git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    echo ${BLUE}Installation du thème powerlevel10k${RESET}
+    [[ -d ~/.oh-my-zsh/custom/themes/powerlevel10k ]] && echo ${GREEN}powerlevel10k déjà installé${RESET} || git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
     
-    echo Désactiver le bruit lors de la recherche
+    echo ${BLUE}Désactiver le bruit lors de la recherche${RESET}
     if grep -Fxq "blacklist pcspkr" /etc/modprobe.d/nobeep.conf;
-    then echo "Blacklist pcspkr déjà configuré"
+    then echo ${GREEN}"Blacklist pcspkr déjà configuré"${RESET}
     else echo "blacklist pcspkr" | sudo tee -a /etc/modprobe.d/nobeep.conf > /dev/null
     fi
     if grep -Fxq "blacklist snd_pcsp" /etc/modprobe.d/nobeep.conf;
-    then echo "Blacklist snd_pcsp déjà configuré"
+    then echo ${GREEN}"Blacklist snd_pcsp déjà configuré"${RESET}
     else echo "blacklist snd_pcsp" | sudo tee -a /etc/modprobe.d/nobeep.conf > /dev/null
     fi
     
-    echo Activation du pavé numérique pour SDDM
+    echo ${BLUE}Activation du pavé numérique pour SDDM${RESET}
     if grep -Fxq "Numlock=on" /etc/sddm.conf;
-    then echo "Pavé numérique déjà configuré"
+    then echo ${GREEN}"Pavé numérique déjà configuré"${RESET}
     else echo "[General]" | sudo tee -a /etc/sddm.conf > /dev/null && echo "Numlock=on" | sudo tee -a /etc/sddm.conf > /dev/null
     fi
     
-    echo Syncthing au démarrage
+    echo ${BLUE}Syncthing au démarrage${RESET}
     sudo systemctl --user enable syncthing.service
     sudo systemctl --user start syncthing.service
     
-    echo Config bash et zsh
+    echo ${BLUE}Config bash et zsh${RESET}
     if grep -Fxq "if [ -f ~/.bash_aliases ]; then" ~/.bashrc;
     then
-        echo config bash ok
+        echo ${GREEN}config bash ok${RESET}
     else
         echo "if [ -f ~/.bash_aliases ]; then . ~/.bash_aliases; fi" | sudo tee -a ~/.bashrc > /dev/null
     fi
     
     if grep -Fxq "source $HOME/.bash_aliases" ~/.zshrc;
-    then echo source bash_aliases déjà ok
+    then echo source bash_aliases déjà ok${RESET}
     else echo "source $HOME/.bash_aliases" | sudo tee -a ~/.zshrc > /dev/null && echo bash_aliases ajouté dans le fichier de configuration .zshrc
     fi
     
     if grep -Fxq "alias lsl" ~/.zshrc;
-    then echo "l'alias lsl existe déjà"
+    then echo ${GREEN}"l'alias lsl existe déjà"${RESET}
     else echo "alias lsl='eza -la --color=always --group-directories-first'" | sudo tee -a ~/.zshrc > /dev/null
     fi
     
     sed -i 's/^ZSH_THEME.*$/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/' ~/.zshrc
     sed -i 's/^plugins=(git).*$/plugins=(\ngit\nzsh-autosuggestions\nzsh-syntax-highlighting\n)/' ~/.zshrc
     
-    echo Nettoyage de tuxinstall
+    echo ${BLUE}Nettoyage de tuxinstall${RESET}
     rm -rf ~/tuxinstall
 else
     echo "Pas de soucis, on s'arrête là :-)"
