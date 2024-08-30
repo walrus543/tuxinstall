e9257e6c221551c765be3e7330dbd5553b8b3632
walrus <github.urljk@8alias.com>
2024-08-30 10:21:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -722,9 +722,9 @@ if check_pkg alacritty && [[ ! -f ~/.config/alacritty/alacritty.toml ]]; then
     fi
 fi
 
-if check_pkg vim && [[ $(grep -c "syntax" ~/.vimrc) -lt 1 ]]; then
+if check_pkg vim && [[ $(grep -c "syntax" /home/$SUDO_USER/.vimrc) -lt 1 ]]; then
     echo -n "- - - .vimrc : "
-    cp "$ICI/config/vimrc" ~/.vimrc
+    cp "$ICI/config/vimrc" /home/$SUDO_USER/.vimrc
     check_cmd
 fi
 
