952e589de39b4f3887564ce06ac519dcbb42a95d
walrus543 <github.urljk@8alias.com>
2025-01-17 09:14:35 +0100

    Diverses modifications

--- run_as_root_first.sh
+++ run_as_root_first.sh
@@ -103,39 +103,69 @@ if [[ $(grep -c "DIFFPROG=/usr/bin/meld" /etc/environment) -lt 1 ]]; then
     check_cmd
 fi
 
-#########################
-# Uncomplicated FireWall
-#########################
-msg_bold_blue "➜ Pare-Feu UFW"
-if check_pkg ufw && [[ $(ufw status | grep -c active) -lt 1 ]]; then
-    echo -n " - - Paramétrage des règles : "
-    ufw reset >> "$log_file" 2>&1
-    ufw default deny incoming >> "$log_file" 2>&1
-    ufw default allow outgoing >> "$log_file" 2>&1
-    ufw allow to 192.168.1.0/24 >> "$log_file" 2>&1
-    ufw allow from 192.168.1.0/24 >> "$log_file" 2>&1
-    ufw deny 22  >> "$log_file" 2>&1 # SSH - uniquement local autorisé
-    ufw enable >> "$log_file" 2>&1
-    systemctl enable --now ufw.service >> "$log_file" 2>&1
-    check_cmd
 
-#    if [[ $(grep -c 'IPV6=no' /etc/default/ufw) -lt 1 ]]; then
-#        echo -n "- - Désactivation IPV6 : "
-#        sed -i sed -i 's/^IPV6=.*/IPV6=no/' /etc/default/ufw
-#        check_cmd
-#	ufw reload
-#    fi
+#++++++++++++++++++++++++++++++++++++++
+# [DEBUT] CHOIX COMPLÈTE OU LITE
+#++++++++++++++++++++++++++++++++++++++
+msg_bold_yellow "********************************\nInstallation complète ou lite ?\n********************************"
 
-    if [[ $(grep -c 'DEFAULT_FORWARD_POLICY=ACCEPT' /etc/default/ufw) -lt 1 ]]; then
-        echo -n "- - Autoriser la police de transfert (VPN...) : "
-        sed -i sed -i 's/^DEFAULT_FORWARD_POLICY=.*/DEFAULT_FORWARD_POLICY=ACCEPT/' /etc/default/ufw
-        check_cmd
+choix=""
+install_type=""
+
+while [ "$choix" != "1" ] && [ "$choix" != "2" ]; do
+
+    echo "1) Complète"
+    echo "2) Lite"
+    echo
+    read -p "Entrez votre choix (1 ou 2) : " choix
+
+    if [ "$choix" = "1" ]; then
+        install_type=1
+    elif [ "$choix" = "2" ]; then
+        install_type=2
+    else
+        msg_bold_red "Choix invalide."
+        echo
+        install_type=""
     fi
+done
 
-    ufw reload
+#--------------------------------------
+# [FIN] CHOIX COMPLÈTE OU LITE
+#--------------------------------------
+
+if [ "$install_type" = 1 ]; then
+    msg_bold_blue "➜ Pare-Feu UFW"
+    pacman -S --needed --noconfirm ufw > /dev/null
+
+    if check_pkg ufw && [[ $(ufw status | grep -c active) -lt 1 ]]; then
+        echo -n " - - Paramétrage des règles : "
+        ufw reset >> "$log_file" 2>&1
+        ufw default deny incoming >> "$log_file" 2>&1
+        ufw default allow outgoing >> "$log_file" 2>&1
+        ufw allow to 192.168.1.0/24 >> "$log_file" 2>&1
+        ufw allow from 192.168.1.0/24 >> "$log_file" 2>&1
+        ufw deny 22  >> "$log_file" 2>&1 # SSH - uniquement local autorisé
+        ufw enable >> "$log_file" 2>&1
+        systemctl enable --now ufw.service >> "$log_file" 2>&1
+        check_cmd
+
+    #    if [[ $(grep -c 'IPV6=no' /etc/default/ufw) -lt 1 ]]; then
+    #        echo -n "- - Désactivation IPV6 : "
+    #        sed -i sed -i 's/^IPV6=.*/IPV6=no/' /etc/default/ufw
+    #        check_cmd
+    #	ufw reload
+    #    fi
+
+        if [[ $(grep -c 'DEFAULT_FORWARD_POLICY=ACCEPT' /etc/default/ufw) -lt 1 ]]; then
+            echo -n "- - Autoriser la police de transfert (VPN...) : "
+            sed -i sed -i 's/^DEFAULT_FORWARD_POLICY=.*/DEFAULT_FORWARD_POLICY=ACCEPT/' /etc/default/ufw
+            check_cmd
+        fi
+
+        ufw reload
+    fi
 fi
-
-
 
 msg_bold_green "Opérations terminées."
 echo "Prêt pour lancer le fichier install.sh"
