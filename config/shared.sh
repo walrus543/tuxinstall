c5b52b8ff52758dd14b090d55c669f4a08d8a67d
walrus543 <github.urljk@8alias.com>
2025-07-19 15:45:14 +0200

    Diverses modifications

--- config/shared.sh
+++ config/shared.sh
@@ -2,11 +2,17 @@
 
 #Coloration du texte
 RESET=$(tput sgr0)
+RESET_ALT="\x1b[0m"
 RED=$(tput setaf 1)
+RED_ALT="\x1b[38;2;255;18;18m"
 GREEN=$(tput setaf 2)
+GREEN_ALT="\x1b[38;2;32;226;1m" #RGB : 32, 226, 1
 YELLOW=$(tput setaf 3)
+YELLOW_ALT="\x1b[38;2;255;212;18m"
 BLUE=$(tput setaf 4)
+BLUE_ALT="\x1b[38;2;64;129;249m"
 BOLD=$(tput bold)
+BOLD_ALT="\x1b[1m"
 sign_green="${GREEN}${BOLD}[+]${RESET}"
 sign_red="${RED}${BOLD}[-]${RESET}"
 
@@ -17,16 +23,22 @@ sleeplong=6
 
 # Fonctions
 msg_bold_yellow() {
-    printf "\n${YELLOW}${BOLD}$1${RESET}\n"
+    printf "\n${YELLOW_ALT}${BOLD_ALT}$1${RESET_ALT}\n"
 }
 msg_bold_blue() {
-    printf "\n${BLUE}${BOLD}$1${RESET}\n"
+    printf "\n${BLUE_ALT}${BOLD_ALT}$1${RESET_ALT}\n"
 }
 msg_bold_green() {
-    printf "\n${GREEN}${BOLD}$1${RESET}\n"
+    printf "\n${GREEN_ALT}${BOLD_ALT}$1${RESET_ALT}\n"
 }
 msg_bold_red() {
-    printf "\n${RED}${BOLD}$1${RESET}\n"
+    printf "\n${RED_ALT}${BOLD_ALT}$1${RESET_ALT}\n"
+}
+msg_ok_green() {
+    printf "${GREEN_ALT}${BOLD_ALT}$1${RESET_ALT}\n"
+}
+msg_ok_red() {
+    printf "${RED_ALT}${BOLD_ALT}$1${RESET_ALT}\n"
 }
 
 ask_continue() {
@@ -42,8 +54,23 @@ ask_continue() {
 check_cmd()
 {
 if [[ $? -eq 0 ]]; then
-    echo ${GREEN}${BOLD}"OK"${RESET}
+    msg_ok_green "OK"
 else
-    echo ${RED}${BOLD}"ERREUR"${RESET}
+    msg_ok_red "ERREUR"
 fi
+}
+
+check_cmd_secure()
+{
+if [[ $? -eq 0 ]]; then
+    msg_ok_green "OK"
+else
+    msg_ok_red "ERREUR"
+    exit 1
+fi
+}
+
+check_pkg()
+{
+	pacman -Q "$1" > /dev/null 2>&1
 }
--- config/uarch.sh
+++ config/uarch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source "$HOME/Documents/Linux/Divers_Scripts/shared.sh"
+source "$HOME"/Documents/Linux/Divers_Scripts/shared.sh
 
 msg_bold_blue "MISE À JOUR PACMAN & AUR..."
     paru
@@ -16,3 +16,16 @@ paru -c
 msg_bold_blue "MISE À JOUR POWERLEVEL10K..."
 git -C ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k pull 
 
+if check_pkg rust; then # rust et donc cargo installé
+    if [[ -n $(cargo install --list) ]]; then # des binaires sont installés
+        if [[ -f "$HOME"/.cargo/bin/cargo-install-update ]]; then #Cargo installé
+            if [[ $(cargo install-update -a | awk '{print $4}' | grep 'yes') -gt 0 ]]; then #MaJ disponibles
+                msg_bold_blue "MISE À JOUR CARGO/RUST PACKAGES..."
+                cargo install-update -a
+            fi
+        else
+            msg_bold_blue "INSTALLATION DE CARGO-UPDATE"
+            cargo install cargo-update
+        fi
+    fi
+fi
